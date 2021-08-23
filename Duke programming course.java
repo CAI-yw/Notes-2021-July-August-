36eb463e865dda67aa60a3f7a4fc98bd974af957
CAI-yw <79295684+CAI-yw@users.noreply.github.com>
2021-08-23 18:34:02 +0800

    Update Duke programming course.java

--- Duke programming course.java
+++ Duke programming course.java
@@ -8,17 +8,17 @@ public class debug1 {
             if (index == -1 || index >= input.length() - 3) {
                 break;
             }
+            System.out.println("index" + index);
             String found = input.substring(index+1, index+4);
-            System.out.println(index + 1);
-            System.out.println(index + 4);
             System.out.println(found);
-            index = input.indexOf("abc", index+4);
+            index = input.indexOf("abc", index+3);
+            System.out.println("index after updating " + index);
         }
    }
    public void test() {
     //no code yet
     //findAbc("abcd");
-    findAbc("abcdabc");
+    findAbc("abcabcabcabca"); // bca, bca, bca, bca
    }
 }
 
